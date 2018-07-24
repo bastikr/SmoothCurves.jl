050b0c5270616f5fa7f66b3eaffe8982866afb07
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-24 15:33:38 +0200

    Fix bugs and improve dynamics.jl

--- src/dynamics.jl
+++ src/dynamics.jl
@@ -6,31 +6,19 @@ end
 
 s_ω(p::DynamicParameters) = p.vmax/p.ωmax
 
-function vmax(params::DynamicParameters, C::Line, vend::Float64, s::Float64)
+function vmax_backward(params::DynamicParameters, C::Line, vend::Float64, s::Float64)
     Δl =  length(C) - curves.l(C, s)
     min(params.vmax, sqrt(vend^2 + 2*params.amax*Δl))
 end
 
-function vmax(params::DynamicParameters, C::Arc, vend::Float64, s::Float64)
+function vmax_backward(params::DynamicParameters, C::Arc, vend::Float64, s::Float64)
     Δl =  length(C) - curves.l(C, s)
     κ = curves.curvature(C)
-    vmax_circle = 1./sqrt(1./params.vmax^2 + (κ/params.wmax)^2)
-    min(vmax_circle, sqrt(vend^2 + 2*params.amax*Δl))
+    vmax_local = 1./sqrt(1./params.vmax^2 + (κ/params.wmax)^2)
+    min(vmax_local, sqrt(vend^2 + 2*params.amax*Δl))
 end
 
-function vmax(params::DynamicParameters, C::PolyCurve, vend::Float64, s::Float64)
-    i = curves.curveindex(C, s)
-    for c=C.curves[end:-1:i+1]
-        vend = vmax(params, c, vend, 0.)
-    end
-    s_i = s
-    if (i>1)
-        s_i -= C.cum_s[i-1]
-    end
-    vmax(params, C.curves[i], vend, s_i)
-end
-
-function vmax(params::DynamicParameters, C::Clothoid, vend::Float64, s::Float64)
+function vmax_backward(params::DynamicParameters, C::Clothoid, vend::Float64, s::Float64)
     smax = curves.smax(C)
     κ = curves.curvature(C, smax)
     vend_local = 1./sqrt(1./params.vmax^2 + (κ/params.wmax)^2)
@@ -42,6 +30,18 @@ function vmax(params::DynamicParameters, C::Clothoid, vend::Float64, s::Float64)
     min(vmax_local, sqrt(vend^2 + 2*params.amax*Δl))
 end
 
+function vmax_backward(params::DynamicParameters, C::PolyCurve, vend::Float64, s::Float64)
+    i = curves.curveindex(C, s)
+    for c=C.curves[end:-1:i+1]
+        vend = vmax_backward(params, c, vend, 0.)
+    end
+    s_i = s
+    if (i>1)
+        s_i -= C.cum_s[i-1]
+    end
+    vmax_backward(params, C.curves[i], vend, s_i)
+end
+
 function vmax_forward(params::DynamicParameters, C::Line, vstart::Float64, s::Float64)
     Δl =  curves.l(C, s)
     min(params.vmax, sqrt(vstart^2 + 2*params.amax*Δl))
@@ -50,8 +50,8 @@ end
 function vmax_forward(params::DynamicParameters, C::Arc, vstart::Float64, s::Float64)
     Δl =  curves.l(C, s)
     κ = curves.curvature(C)
-    vmax_circle = 1./(1./params.vmax^2 + (κ/params.wmax)^2)
-    min(vmax_circle, vstart^2 + 2*params.amax*Δl)
+    vmax_circle = 1./sqrt(1./params.vmax^2 + (κ/params.wmax)^2)
+    min(vmax_circle, sqrt(vstart^2 + 2*params.amax*Δl))
 end
 
 function vmax_forward(params::DynamicParameters, C::Clothoid, vstart::Float64, s::Float64)
