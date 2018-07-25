0fc2716a77613a69e61a371ec1a4248b05b34cb9
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-25 06:51:10 +0200

    Improve clothoid code.

--- src/clothoid.jl
+++ src/clothoid.jl
@@ -14,21 +14,8 @@ struct Clothoid <: Curve
 end
 
 
-# Implement Curve interface
-smax(C::Clothoid) = C.l1 - C.l0
-
-length(C::Clothoid, s::Real) = s
-length(C::Clothoid) = length(C, smax(C))
-dlength(C::Clothoid, s::Real) = 1.
-
-tangentangle(C::Clothoid, s::Real) = normalize_angle(C.rotation + C.λ*(C.l0 + s)^2)
-
-curvature(C::Clothoid, s::Real) = 2*C.λ*(C.l0 + s)
-dcurvature(C::Clothoid, s::Real) = 2*C.λ
-
-function smax_from_deviation(λ, dphi)
-    sqrt(abs(dphi/(2*λ)))
-end
+# Clothoid specific functions
+l(C::Clothoid, s::Real) = C.l0 + s
 
 function Fcos(λ::Float64, s::Float64)
     return QuadGK.quadgk(x->cos(λ*x^2), 0., s)[1]::Float64
@@ -46,18 +33,35 @@ function Fresnel(λ::Float64, s::Real)
     return QuadGK.quadgk(f, 0., s)[1]::SVector{2, Float64}
 end
 
+
+# Implement Curve interface
+smax(C::Clothoid) = C.l1 - C.l0
+
+length(C::Clothoid, s::Real) = s
+dlength(C::Clothoid, s::Real) = 1.
+
+tangentangle(C::Clothoid, s::Real) = normalize_angle(C.rotation + C.λ*l(C, s)^2)
+
+curvature(C::Clothoid, s::Real) = 2*C.λ*l(C, s)
+dcurvature(C::Clothoid, s::Real) = 2*C.λ
+
 function point(C::Clothoid, s::Real)
-    p = Fresnel(C.λ, C.l0+s)::SVector{2, Float64}
+    p = Fresnel(C.λ, l(C, s))::SVector{2, Float64}
     p_r = rotate2d(C.rotation, p[1] - C.shift, p[2])::SVector{2, Float64}
     return p_r + C.origin
 end
 
 function dpoint(C::Clothoid, s::Real)
-    l_ = C.l0+s
+    l_ = l(C, s)
     dx = cos(C.λ*l_^2)
     dy = sin(C.λ*l_^2)
-    dx_r, dy_r = rotate2d(C.rotation, dx, dy)::SVector{2, Float64}
-    return SVector{2, Float64}(dx_r, dy_r)
+    return rotate2d(C.rotation, dx, dy)::SVector{2, Float64}
+end
+
+
+# Construction functions
+function smax_from_deviation(λ, dphi)
+    sqrt(abs(dphi/(2*λ)))
 end
 
 function construct_clothoids(λ::Real, p0, v0, v1)
