d80e60ab1d5d19ada8e5f0dde2e6d359ff8e5b76
Sebastian Kraemer <basti.kr@gmail.com>
2018-07-31 06:37:28 +0200

    Add new construction method.

--- src/construction.jl
+++ src/construction.jl
@@ -44,6 +44,44 @@ function construct_clothoids2(dmax::Real, p0, p1, p2)
     return C0, C1
 end
 
+function construct_clothoids3(dmax::Real, p0, p1, p2)
+    p0_ = convert(SVector{2, Float64}, p0)
+    p1_ = convert(SVector{2, Float64}, p1)
+    p2_ = convert(SVector{2, Float64}, p2)
+
+    v0 = p1 - p0
+    v1 = p2 - p1
+
+    u0 = normalize(v0)
+    u1 = normalize(v1)
+
+    Δφ = deviation(u0, u1)
+
+    maxshift = min(norm(v0), norm(v1))
+    θ = abs(Δφ/2)
+    Fcos, Fsin = Fresnel(1., sqrt(θ))
+
+    ν = sin(θ) + cos(θ)*Fcos/Fsin
+    dmax_shift = maxshift/ν
+
+    if dmax_shift<dmax
+        d = dmax_shift
+        shift = maxshift
+    else
+        d = dmax
+        shift = ν*d
+    end
+    smax = d*sqrt(θ)*cos(θ)/Fsin
+    λ = sign(Δφ)*θ/smax^2
+
+    phi0 = deviation(u0, [1, 0])
+    phi1 = deviation(u1, [1, 0])
+
+    C0 = Clothoid(p1, shift, -phi0, λ, 0., smax)
+    C1 = Clothoid(p1, -shift, -phi1, -λ, -smax, 0.)
+    return PolyCurve(C0, C1)
+end
+
 
 function construct_curve(λ, p0, p1, p2)
     v0 = p1 - p0
@@ -67,4 +105,30 @@ function construct_curve2(dmax, p0, p1, p2)
     dp1 = p2 - p1_
     L1 = Line(p1_, dp1, norm(dp1))
     PolyCurve(L0, C0, C1, L1)
+end
+
+function construct_curve3(dmax, points)
+    curves = Curve[]
+    p2_ = points[1]
+    for i=1:length(points)-2
+        p0 = p2_
+        p1 = points[i+1]
+        if (i+2>length(points))
+            p2 = points[end]
+        else
+            p2 = 0.5*(points[i+1] + points[i+2])
+        end
+        C = construct_clothoids3(dmax, p0, p1, p2)
+        p0_ = point(C, 0)
+        if norm(p0_ - p0) > 1e-12
+            push!(curves, Line(p0, p0_-p0, norm(p0_-p0)))
+        end
+        push!(curves, C.curves[1])
+        push!(curves, C.curves[2])
+        p2_ = point(C, smax(C))
+    end
+    if norm(p2_ - points[end]) > 1e-12
+        push!(curves, Line(p2_, points[end]-p2_, norm(p2_-points[end])))
+    end
+    return PolyCurve(curves...)
 end
