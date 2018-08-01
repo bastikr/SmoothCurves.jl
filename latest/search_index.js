85a81737a9acf09eb7bd8e3b48cfe19e93a63709
autodocs <autodocs>
2018-08-01 16:09:45 +0000

    build based on ebe4e47

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "SmoothCurves.jl",
     "title": "Curve Types",
     "category": "section",
-    "text": "Curve\nClothoid\nArc"
+    "text": "Curve\nClothoid\nArcSegment"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Clothoid",
     "title": "Clothoid",
     "category": "section",
-    "text": "using SmoothCurves # hide\n\nusing Plots # hide\n\ndmax = 1.5 # hide\np0 = [-6, 0] # hide\np1 = [0.0, 0.0] # hide\np2 = [2, 5] # hide\nC = SmoothCurves.construct_curve2(dmax, p0, p1, p2) # hide\nn0 = normalize([-(p1[2] - p0[2]), (p1[1] - p0[1])]) # hide\n\nλ = C.curves[2].λ # hide\nphi = SmoothCurves.deviation(p1-p0, p2-p1) # hide\nθ = phi/2 # hide\nFcos, Fsin = SmoothCurves.Fresnel(1, sqrt(θ)) # hide\nsmax_ = sqrt(abs(θ/λ)) # hide\nΔy = smax_/sqrt(θ)*Fsin # hide\nΔx = smax_/sqrt(θ)*Fcos # hide\nδ = Δy*tan(θ) # hide\nd = sqrt(δ^2 + Δy^2) # hide\n\nplt = plot(ticks=false, border=false, axis=false) # hide\n\n# Plot clothoids # hide\nplot!(C, lc=1, lw=2) # hide\np_begin = point(C.curves[2], 0) # hide\np_split = point(C.curves[3], 0) # hide\np_end = point(C.curves[4], 0) # hide\nscatter!([p_begin[1], p_split[1], p_end[1]], [p_begin[2], p_split[2], p_end[2]], # hide\n        color=1, label=\"\") # hide\n\n# Plot polygon lines # hide\nC_l0 = Line(p0, p1-p0, norm(p1-p0)) # hide\nC_l1 = Line(p1, p2-p1, norm(p2-p1)) # hide\nplot!(C_l0, lc=:black) # hide\nplot!(C_l1, lc=:black) # hide\n\np_annotate = p1 - (δ + 0.5*Δx)*C_l0.direction - 0.2*n0 # hide\nannotate!(p_annotate..., text(\"\\\\Delta x\", 8, :center)) # hide\n\np_annotate = p1 - 0.5*δ*C_l0.direction - 0.2*n0 # hide\nannotate!(p_annotate..., text(\"\\\\delta\", 8, :center)) # hide\n\nC_l0_extension = Line(p1, p1-p0, dot(C_l0.direction, p2-p1)) # hide\nplot!(C_l0_extension, lc=:black, ls=:dash) # hide\n\n# Plot normal to first line # hide\nC_l0_normal = Line(p1 - δ*C_l0.direction, n0, Δy) # hide\nplot!(C_l0_normal, lc=:black, ls=:solid) # hide\np_annotate = C_l0_normal.origin - 0.2*C_l0.direction + 0.5*n0*C_l0_normal.length # hide\nannotate!(p_annotate..., text(\"\\\\Delta y\", 8, :center)) # hide\n\n# Plot mindistance # hide\nC_l_mindistance = Line(p1, -C_l0.direction + C_l1.direction, sqrt(δ^2 + Δy^2)) # hide\nplot!(C_l_mindistance, lc=:black, ls=:solid) # hide\np_annotate = p1 + 0.5*C_l_mindistance.direction*C_l_mindistance.length + [0.1, 0.1] # hide\nannotate!(p_annotate..., text(\"d\", 8, :center)) # hide\n\n# Plot theta angle # hide\nr_thetaangle = 0.3 # hide\nC_thetaangle = Arc(p1 + C_l_mindistance.length*C_l_mindistance.direction, r_thetaangle, # hide\n    SmoothCurves.deviation(C_l0.direction, -n0), # hide\n    SmoothCurves.deviation(C_l0.direction, -C_l_mindistance.direction)) # hide\nplot!(C_thetaangle, lc=:black, ls=:solid) # hide\np_annotate = C_thetaangle.origin - (r_thetaangle+0.2)*normalize(C_l_mindistance.direction + n0) # hide\nannotate!(p_annotate..., text(\"\\\\theta\", 8, :center)) # hide\n\n# Plot phi angle # hide\nr_phiangle = 0.5 # hide\nC_phiangle = Arc(p1, r_phiangle, tangentangle(C_l0, 0.), tangentangle(C_l1, 0.)) # hide\nplot!(C_phiangle, lc=:black, ls=:dash) # hide\np_annotate = p1 + (r_phiangle + 0.2)*normalize(C_l0.direction + C_l1.direction) # hide\nannotate!(p_annotate..., text(\"\\\\phi\", 8, :center)) # hide"
+    "text": "using SmoothCurves # hide\n\nusing Plots # hide\n\ndmax = 1.5 # hide\np0 = [-6, 0] # hide\np1 = [0.0, 0.0] # hide\np2 = [2, 5] # hide\nC = SmoothCurves.construct_curve2(dmax, p0, p1, p2) # hide\nn0 = normalize([-(p1[2] - p0[2]), (p1[1] - p0[1])]) # hide\n\nλ = C.curves[2].λ # hide\nphi = SmoothCurves.deviation(p1-p0, p2-p1) # hide\nθ = phi/2 # hide\nFcos, Fsin = SmoothCurves.Fresnel(1, sqrt(θ)) # hide\nsmax_ = sqrt(abs(θ/λ)) # hide\nΔy = smax_/sqrt(θ)*Fsin # hide\nΔx = smax_/sqrt(θ)*Fcos # hide\nδ = Δy*tan(θ) # hide\nd = sqrt(δ^2 + Δy^2) # hide\n\nplt = plot(ticks=false, border=false, axis=false) # hide\n\n# Plot clothoids # hide\nplot!(C, lc=1, lw=2) # hide\np_begin = point(C.curves[2], 0) # hide\np_split = point(C.curves[3], 0) # hide\np_end = point(C.curves[4], 0) # hide\nscatter!([p_begin[1], p_split[1], p_end[1]], [p_begin[2], p_split[2], p_end[2]], # hide\n        color=1, label=\"\") # hide\n\n# Plot polygon lines # hide\nC_l0 = LineSegment(p0, p1) # hide\nC_l1 = LineSegment(p1, p2) # hide\nplot!(C_l0, lc=:black) # hide\nplot!(C_l1, lc=:black) # hide\n\np_annotate = p1 - (δ + 0.5*Δx)*direction(C_l0) - 0.2*n0 # hide\nannotate!(p_annotate..., text(\"\\\\Delta x\", 8, :center)) # hide\n\np_annotate = p1 - 0.5*δ*direction(C_l0) - 0.2*n0 # hide\nannotate!(p_annotate..., text(\"\\\\delta\", 8, :center)) # hide\n\nl = length(C_l1)*dot(direction(C_l0), direction(C_l1)) #hide\nC_l0_extension = LineSegment(p1, l*direction(C_l0)) # hide\nplot!(C_l0_extension, lc=:black, ls=:dash) # hide\n\n# Plot normal to first line # hide\np_ = p1 - δ*direction(C_l0) # hide\nC_l0_normal = LineSegment(p_, p_ + Δy*n0) # hide\nplot!(C_l0_normal, lc=:black, ls=:solid) # hide\np_annotate = point(C_l0_normal, 0) - 0.2*direction(C_l0) + 0.5*n0*length(C_l0_normal) # hide\nannotate!(p_annotate..., text(\"\\\\Delta y\", 8, :center)) # hide\n\n# Plot mindistance # hide\nC_l_mindistance = LineSegment(p1, p1 - δ*direction(C_l0) + Δy*direction(C_l0_normal)) # hide\nplot!(C_l_mindistance, lc=:black, ls=:solid) # hide\np_annotate = p1 + 0.5*direction(C_l_mindistance)*length(C_l_mindistance) + [0.1, 0.1] # hide\nannotate!(p_annotate..., text(\"d\", 8, :center)) # hide\n\n# Plot theta angle # hide\nr_thetaangle = 0.3 # hide\nC_thetaangle = ArcSegment(p1 + length(C_l_mindistance)*direction(C_l_mindistance), r_thetaangle, # hide\n    SmoothCurves.deviation(direction(C_l0), -n0), # hide\n    SmoothCurves.deviation(direction(C_l0), -direction(C_l_mindistance))) # hide\nplot!(C_thetaangle, lc=:black, ls=:solid) # hide\np_annotate = C_thetaangle.origin - (r_thetaangle+0.2)*normalize(direction(C_l_mindistance) + n0) # hide\nannotate!(p_annotate..., text(\"\\\\theta\", 8, :center)) # hide\n\n# Plot phi angle # hide\nr_phiangle = 0.5 # hide\nC_phiangle = ArcSegment(p1, r_phiangle, tangentangle(C_l0, 0.), tangentangle(C_l1, 0.)) # hide\nplot!(C_phiangle, lc=:black, ls=:dash) # hide\np_annotate = p1 + (r_phiangle + 0.2)*normalize(direction(C_l0) + direction(C_l1)) # hide\nannotate!(p_annotate..., text(\"\\\\phi\", 8, :center)) # hide"
 },
 
 {
@@ -73,27 +73,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "arc.html#",
-    "page": "Arc",
-    "title": "Arc",
+    "location": "arcsegment.html#",
+    "page": "ArcSegment",
+    "title": "ArcSegment",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "arc.html#Arc-1",
-    "page": "Arc",
-    "title": "Arc",
+    "location": "arcsegment.html#ArcSegment-1",
+    "page": "ArcSegment",
+    "title": "ArcSegment",
     "category": "section",
     "text": ""
 },
 
 {
-    "location": "arc.html#Additional-Functions-1",
-    "page": "Arc",
+    "location": "arcsegment.html#Additional-Functions-1",
+    "page": "ArcSegment",
     "title": "Additional Functions",
     "category": "section",
-    "text": "Arc"
+    "text": "ArcSegment"
 },
 
 {
@@ -121,11 +121,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api.html#SmoothCurves.Arc",
+    "location": "api.html#SmoothCurves.ArcSegment",
     "page": "API",
-    "title": "SmoothCurves.Arc",
+    "title": "SmoothCurves.ArcSegment",
     "category": "type",
-    "text": "Arc(origin, radius, angle0, angle1)\n\nArc centered at origin with the given radius going from the first to the second angle. As parametrization the angle difference to the starting angle is used.\n\n\n\n"
+    "text": "ArcSegment(origin, radius, angle0, angle1)\n\nArcSegment centered at origin with the given radius going from the first to the second angle. As parametrization the angle difference to the starting angle is used.\n\n\n\n"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "Curve Types",
     "category": "section",
-    "text": "Curve\nArc\nCurve\nArc"
+    "text": "Curve\nArcSegment\nCurve\nArcSegment"
 },
 
 {
@@ -217,19 +217,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api.html#Base.sign-Tuple{SmoothCurves.Arc}",
+    "location": "api.html#Base.sign-Tuple{SmoothCurves.ArcSegment}",
     "page": "API",
     "title": "Base.sign",
     "category": "method",
-    "text": "sign(C::Arc)\n\nSign of the curvature of the Arc.\n\n\n\n"
+    "text": "sign(C::ArcSegment)\n\nSign of the curvature of the ArcSegment.\n\n\n\n"
 },
 
 {
-    "location": "api.html#API-Arc-1",
+    "location": "api.html#API-ArcSegment-1",
     "page": "API",
-    "title": "Arc",
+    "title": "ArcSegment",
     "category": "section",
-    "text": "sign(C::Arc)"
+    "text": "sign(C::ArcSegment)"
 },
 
 ]}
