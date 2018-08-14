c48b12e525dfe1088b430d733092be9f062a42ef
Sebastian Kraemer <basti.kr@gmail.com>
2018-08-14 18:47:30 +0200

    Use power series to calculate fresnel integral.

--- src/clothoid.jl
+++ src/clothoid.jl
@@ -38,6 +38,53 @@ Signed distance to the origin of the Clothoid.
 """
 standardlength(C::Clothoid, s::Real) = C.l0 + s
 
+
+function fresnel_powerseries(λ::Real, s::Real)
+    λ_ = convert(Float64, λ)
+    θ = abs(λ_)*s^2
+    if (θ<1e-16)
+        return SVector(s, 0.)
+    elseif (θ>π/2+0.01)
+        throw(ArgumentError("θ=λ*s^2=$θ is not in [-π/2, π/2]."))
+    end
+    sqrt_λ = sqrt(abs(λ_))
+    s_ = sqrt_λ*convert(Float64, s)
+    s4 = s_^4
+
+    cx0 =  1
+    cx1 = -1/10
+    cx2 =  1/216
+    cx3 = -1/9360
+    cx4 =  1/685440
+    cx5 = -1/76204800
+    cx6 =  1/11975040000
+    cx7 = -1/2528170444800
+    x = s_*(cx0 + s4*(cx1 + s4*(cx2 + s4*(cx3 + s4*(cx4 + s4*(cx5 + s4*(cx6 + s4*cx7)))))))
+
+    cy0 =  1/3
+    cy1 = -1/42
+    cy2 =  1/1320
+    cy3 = -1/75600
+    cy4 =  1/6894720
+    cy5 = -1/918086400
+    cy6 =  1/168129561600
+    cy7 = -1/40537905408000
+    y = s_^3*(cy0 + s4*(cy1 + s4*(cy2 + s4*(cy3 + s4*(cy4 + s4*(cy5 + s4*(cy6 + s4*cy7)))))))
+
+    return SVector(x/sqrt_λ, sign(λ_)*y/sqrt_λ)
+end
+
+
+function fresnel_numericintegration(λ::Real, s::Real)
+    λ_ = convert(Float64, λ)
+    s_ = convert(Float64, s)
+    function f(x::Float64)
+        t = λ_*x^2
+        SVector{2, Float64}(cos(t), sin(t))
+    end
+    QuadGK.quadgk(f, 0., s_)[1]::SVector{2, Float64}
+end
+
 doc"""
     fresnel(λ, s)
 
@@ -49,15 +96,7 @@ x(s) = \int_0^s \cos(λ t^2) \mathrm{d}t
 y(s) = \int_0^s \sin(λ t^2) \mathrm{d}t
 ```
 """
-function fresnel(λ::Real, s::Real)
-    λ_ = convert(Float64, λ)
-    s_ = convert(Float64, s)
-    function f(x::Float64)
-        t = λ_*x^2
-        SVector{2, Float64}(cos(t), sin(t))
-    end
-    QuadGK.quadgk(f, 0., s_)[1]::SVector{2, Float64}
-end
+fresnel(λ::Real, s::Real) = fresnel_powerseries(λ, s)
 
 
 # Implement Curve interface
